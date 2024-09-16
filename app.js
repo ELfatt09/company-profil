11a36b71fd32c748e0fa4e5d9abc83a78b0e0539
Sevalino Elfata <sevalinoelfata8@gmail.com>
2024-09-16 13:09:00 +0700

    commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+
+    const items = document.querySelectorAll('.grid-item, .testimonial, .team-member');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target); // Stop observing once the animation is triggered
+            }
+        });
+    }, { threshold: 0.2 });
+
+    items.forEach(item => {
+        observer.observe(item);
+    });
+});
